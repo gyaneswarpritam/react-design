140b2743deff41cb5e0f1510849b0c1825abbcf1
Pritam <gyaneswar.pritam@gmail.com>
2024-01-11 00:38:40 +0530

    change

--- src/components/MobileFooter.js
+++ src/components/MobileFooter.js
@@ -1,30 +1,54 @@
 // MobileFooter.js
-import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHome, faList, faEnvelope, faShoppingCart, faEllipsisH } from '@fortawesome/free-solid-svg-icons';
-import "./MobileFooter.css";
+import React, { useState } from 'react';
+import HomeIcon from '@material-ui/icons/Home';
+import ListIcon from '@material-ui/icons/List';
+import EmailIcon from '@material-ui/icons/Email';
+import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
+import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
+import './MobileFooter.css';
 
 const MobileFooter = () => {
+  const [activeItem, setActiveItem] = useState('home');
+
+  const handleItemClick = (item) => {
+    setActiveItem(item);
+  };
+
   return (
-    <div className="mobile-footer">
-      <div className="footer-item">
-        <FontAwesomeIcon icon={faHome} />
+    <div className={'mobile-footer'}>
+      <div
+        className={`${'footer-item'} ${activeItem === 'home' ? 'active' : ''}`}
+        onClick={() => handleItemClick('home')}
+      >
+        <HomeIcon />
         <span>Home</span>
       </div>
-      <div className="footer-item">
-        <FontAwesomeIcon icon={faList} />
+      <div
+        className={`${'footer-item'} ${activeItem === 'catalog' ? 'active' : ''}`}
+        onClick={() => handleItemClick('catalog')}
+      >
+        <ListIcon />
         <span>Catalog</span>
       </div>
-      <div className="footer-item">
-        <FontAwesomeIcon icon={faEnvelope} />
+      <div
+        className={`${'footer-item'} ${activeItem === 'enquiry' ? 'active' : ''}`}
+        onClick={() => handleItemClick('enquiry')}
+      >
+        <EmailIcon />
         <span>Enquiry</span>
       </div>
-      <div className="footer-item">
-        <FontAwesomeIcon icon={faShoppingCart} />
+      <div
+        className={`${'footer-item'} ${activeItem === 'cart' ? 'active' : ''}`}
+        onClick={() => handleItemClick('cart')}
+      >
+        <ShoppingCartIcon />
         <span>Cart</span>
       </div>
-      <div className="footer-item">
-        <FontAwesomeIcon icon={faEllipsisH} />
+      <div
+        className={`${'footer-item'} ${activeItem === 'more' ? 'active' : ''}`}
+        onClick={() => handleItemClick('more')}
+      >
+        <MoreHorizIcon />
         <span>More</span>
       </div>
     </div>
